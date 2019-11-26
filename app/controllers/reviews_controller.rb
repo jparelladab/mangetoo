b2a599482d892ae296ac92968cb4297962adef8a
Benjamin Baker <bjmn.bkr@gmail.com>
2019-11-26 00:12:49 +0000

    Updated seeds and minor tweak to user model

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,7 +1,6 @@
 class ReviewsController < ApplicationController
   def index
     # NEED TO COMPLETE @REVIEWS JUST FOR THE CURRENT USER
-    @reviews = current_user.visits.reviews
   end
 
   def new
