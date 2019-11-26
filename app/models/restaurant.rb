423ba201c024b37885760d9302591f43a88f170b
Benjamin Baker <bjmn.bkr@gmail.com>
2019-11-26 12:40:24 +0000

    Validates now included in comments until later in the project

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,4 +1,9 @@
 class Restaurant < ApplicationRecord
   has_many :bookmarks
   has_many :visits
+  # UNCOMMENT THE VALIDATIONS BELOW LATER IN THE PROJECT
+  # validates :name, presence: true
+  # validates :address, presence: true
+  # validates :city, presence: true
+  # validates :category, presence: true
 end
