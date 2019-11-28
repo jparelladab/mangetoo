b72a755c7304c347daef953018c93292de844490
Benjamin Baker <bjmn.bkr@gmail.com>
2019-11-28 17:21:02 +0000

    Fixed algolia

--- config/initializers/algoliasearch.rb
+++ config/initializers/algoliasearch.rb
@@ -1,1 +1,1 @@
-AlgoliaSearch.configuration = { ENV['ALGOLIA_APPLICATION_ID'], ENV['ALGOLIA_API_KEY'] }
+AlgoliaSearch.configuration = { application_id: ENV['ALGOLIA_APPLICATION_ID'], api_key: ENV['ALGOLIA_API_KEY'] }
