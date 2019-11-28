0b63c8948c37be34533792b918e525eacc5454fa
Jo <yellowestyellow@gmail.com>
2019-11-28 15:28:39 +0000

    Carousel for index.

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -15,7 +15,16 @@ class RestaurantsController < ApplicationController
         infoWindow: render_to_string(partial: "info_window", locals: { restaurant: restaurant })
       }
     end
-    @restaurants = Restaurant.all if @restaurants.empty?
+    if @restaurants.empty?
+      @restaurants = Restaurant.all
+      @markers = @restaurants.map do |restaurant|
+      {
+        lat: restaurant.latitude,
+        lng: restaurant.longitude,
+        infoWindow: render_to_string(partial: "info_window", locals: { restaurant: restaurant })
+      }
+      end
+    end
   end
 
   def new
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,8 +1,14 @@
 import "bootstrap";
 import { initMapbox } from '../plugins/init_mapbox';
-import 'mapbox-gl/dist/mapbox-gl.css'; // <-- you need to uncomment the stylesheet_pack_tag in the layout!
-import { initAutocomplete } from "../plugins/init_autocomplete"
+import 'mapbox-gl/dist/mapbox-gl.css';
+import { initAutocomplete } from "../plugins/init_autocomplete";
+import createNewGlider from "./glider";
 
 initMapbox();
 
 initAutocomplete();
+
+//Run all Glider Carousels.
+document.querySelectorAll('.glider').forEach((glider) => {
+  createNewGlider(glider.dataset.id)
+})
