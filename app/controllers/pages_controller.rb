60cd351452f83be64b8514d43abb2e38d48e7eb2
Benjamin Baker <bjmn.bkr@gmail.com>
2019-11-28 23:24:51 +0000

    Updated seeds

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,5 @@
 class PagesController < ApplicationController
   def home
-
     followings_id = Follow.where(follower_id: current_user.id).map { |user| user.following_id }
     followings_visits = followings_id.map { |id| Visit.where(user_id: id) }
     followings_visits_ids = followings_visits.map { |visit| visit.ids }.flatten
@@ -10,8 +9,4 @@ class PagesController < ApplicationController
     my_reviews_arrelation = my_visits_ids.map { |id| Review.where(visit_id: id) }
     @my_reviews = my_reviews_arrelation.map { |r| r.first}.compact
   end
-
-
-  private
-
 end
